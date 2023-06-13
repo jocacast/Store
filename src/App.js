b1772bb91eaad6f6620df275ab7b3b2344a0ea7a
jocacast <jcastil91@gmail.com>
2023-06-13 14:53:40 -0400

    Category and categories sections added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./components/category-item/category-item.component.jsx";
+//import "./components/category-item/category-item.component.jsx";
 import Home from "./routes/home/home.component.jsx";
 import { Routes, Route} from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component.jsx";
@@ -11,7 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Navigation/>}>
         <Route index element={<Home />}/>
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} />
       </Route>
